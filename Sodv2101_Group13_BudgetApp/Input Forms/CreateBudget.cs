028249a02262a2ae4664edcdb443a70e95ddf5b0
KailanB <kailanbates@gmail.com>
2024-11-21 00:22:14 -0700

    Create budget form
    Retrieves data from text boxes
    Creates a budget object
    Passes object to BudgetService.Create method.
    Budget service handles CRUD operations

---
+++ Sodv2101_Group13_BudgetApp/Input Forms/CreateBudget.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Sodv2101_Group13_BudgetApp.RepositoryDBContext.BudgetServices;
+
+namespace Sodv2101_Group13_BudgetApp.Input_Forms
+{
+	public partial class CreateBudget : Form
+	{
+
+		// create budget form has a class with methods that run CRUD
+		private BudgetService budgetService = new BudgetService();
+		public CreateBudget()
+		{
+			InitializeComponent();
+		}
+
+		// on create button click
+		private void btnCreate_Click(object sender, EventArgs e)
+		{
+			// retrieve data from form
+			string name = txtBudgetName.Text;
+			double amount = Convert.ToDouble(numBudgetAmount.Value);
+			string descript = txtBudgetDescription.Text;
+
+			// create a new budget class object
+			Budget budget = new Budget(name, amount, descript);
+
+			// pass the object to the create budget method of budget service
+			// proceed to budget service class
+			budgetService.CreateBudget(budget);
+
+			// standard reset values
+			txtBudgetName.Text = "";
+			numBudgetAmount.Value = 0;
+			txtBudgetDescription.Text = "";
+
+
+		}
+	}
+}
