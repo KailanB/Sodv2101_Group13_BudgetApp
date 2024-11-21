20ab08040dd339f75823a728582b43b0771eea91
KailanB <kailanbates@gmail.com>
2024-11-21 00:20:24 -0700

    Created budget form from form1 button

--- Sodv2101_Group13_BudgetApp/Form1.cs
+++ Sodv2101_Group13_BudgetApp/Form1.cs
@@ -1,10 +1,27 @@
+using Microsoft.Data.SqlClient;
+using Sodv2101_Group13_BudgetApp.Input_Forms;
+
 namespace Sodv2101_Group13_BudgetApp
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
-        }
-    }
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+
+
+
+		}
+
+		private void btnCreateBudget_Click(object sender, EventArgs e)
+		{
+
+			// create a new budget form on button click and show it 
+			// proceed to the budget form
+			CreateBudget budgetForm = new CreateBudget();
+			budgetForm.ShowDialog();
+
+
+		}
+	}
 }
