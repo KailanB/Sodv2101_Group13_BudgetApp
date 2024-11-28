d859130eeeb1a84de6fe450d77ff46d401507378
Pixlypops <127553887+Pixlypops@users.noreply.github.com>
2024-11-28 00:05:02 -0700

    Create ContributionService.cs

---
+++ Sodv2101_Group13_BudgetApp/RepositoryDBContext/Contribution/ContributionService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sodv2101_Group13_BudgetApp.RepositoryDBContext.Contribution
+{
+    internal class ContributionService
+    {
+    }
+}
