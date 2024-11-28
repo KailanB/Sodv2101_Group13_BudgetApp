c2940cdd67f193f303d8654dbf84f10c3c0590b7
KailanB <kailanbates@gmail.com>
2024-11-27 23:28:34 -0700

    changed name and access modifier

--- Sodv2101_Group13_BudgetApp/Budget.cs
+++ Sodv2101_Group13_BudgetApp/Budget.cs
@@ -14,7 +14,7 @@ namespace Sodv2101_Group13_BudgetApp
         public string Description { get; private set; }
         public int UserID { get; private set; } 
 
-        public List<Expense> expenses { get; private set; } = new List<Expense>();
+        public List<Expense> Expenses { get; set; } = new List<Expense>();
 
         public Budget()
         {
@@ -40,15 +40,15 @@ namespace Sodv2101_Group13_BudgetApp
 
         public void AddNewExpense(Expense expense)
         {
-            // Expense newExpense = new Expense(name, amount, description, timePeriod);
-            expenses.Add(expense);
+			// Expense newExpense = new Expense(name, amount, description, timePeriod);
+			Expenses.Add(expense);
 
         }
 
         public double GetExpenseTotal(int month)
         {
             double total = 0;
-            foreach(Expense expense in expenses)
+            foreach(Expense expense in Expenses)
             {
                 total += expense.Amount;
             }
@@ -59,7 +59,7 @@ namespace Sodv2101_Group13_BudgetApp
 		public double GetExpenseTotalByMonth(int month)
 		{
 			double total = 0;
-			foreach (Expense expense in expenses)
+			foreach (Expense expense in Expenses)
 			{
                 if(expense.TimePeriod.Month == month)
                 {
