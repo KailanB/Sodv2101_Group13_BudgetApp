0a3ca9d176a50ebdddbe6dec1aa618f85a847f8d
KailanB <kailanbates@gmail.com>
2024-11-27 23:28:51 -0700

    Added budget graph

--- Sodv2101_Group13_BudgetApp/Form1.cs
+++ Sodv2101_Group13_BudgetApp/Form1.cs
@@ -2,6 +2,7 @@ using Microsoft.Data.SqlClient;
 using Sodv2101_Group13_BudgetApp.InputForms;
 using Sodv2101_Group13_BudgetApp.RepositoryDBContext.BudgetServices;
 using Sodv2101_Group13_BudgetApp.SubPageForms;
+using System.Drawing;
 
 namespace Sodv2101_Group13_BudgetApp
 {
@@ -46,18 +47,63 @@ namespace Sodv2101_Group13_BudgetApp
         {
             List<Budget> budgets = budgetService.GetBudgetList();
             int locationYIncrease = 0;
-            foreach(Budget budget in budgets)
+			Bitmap bitmap = new Bitmap(250, 150);
+
+
+			foreach (Budget budget in budgets)
             {
                 Label label = new Label();
                 label.Location = new Point(20, 225 + locationYIncrease);
                 label.Text = budget.Name;
                 this.Controls.Add(label);
-                locationYIncrease += 30;
+                
 
+                // get current date
+                DateTime date = DateTime.Now;
+                // pass month to get only expenses for that month
+                double expensesTotal = budget.GetExpenseTotalByMonth(date.Month);
+				Label labelExpense = new Label();
+                labelExpense.AutoSize = false;
+                labelExpense.Width = 250;
+				labelExpense.Location = new Point(370, 225 + locationYIncrease);
+				labelExpense.Text = $"Max: {budget.Max}. Spent: {expensesTotal}. Remaining: {budget.Max-expensesTotal}";
 
+				this.Controls.Add(labelExpense);
 
-            }
-            //MessageBox.Show(budgets.Count.ToString());
-        }
+                using (Graphics g = Graphics.FromImage(bitmap))
+                {
+                    // calculate percentage spent
+                    double percentSpent = ((expensesTotal * 100) / budget.Max) / 100;
+                   
+                    // define bar of spent amount
+                    int spent = (Int32)(percentSpent * 250);
+                    // define bar of remainder
+                    int remaining = 250 - spent;
+                    // rectangle x axis here start "spent" percent over since that portion will be red
+                    // recntangle width is then remaining amount
+					Rectangle rectRemainder = new Rectangle(0 + spent, 0 + locationYIncrease, remaining, 20);
+                    // rectangle here is simply width of spent
+                    Rectangle rectSpent = new Rectangle(0, 0 + locationYIncrease, spent, 20);
+
+                    // draw green
+                    using (SolidBrush brush = new SolidBrush(Color.FromArgb(0, 255, 0)))
+                    {
+                        g.FillRectangle(brush, rectRemainder);
+                    }
+                    // draw red
+                    using (SolidBrush brush = new SolidBrush(Color.FromArgb(255, 0, 0)))
+                    {
+                        g.FillRectangle(brush, rectSpent);
+                    }
+                }
+
+                
+
+				locationYIncrease += 30;
+			}
+
+		   picBudgetGraphs.Image = bitmap;
+
+		}
     }
 }
