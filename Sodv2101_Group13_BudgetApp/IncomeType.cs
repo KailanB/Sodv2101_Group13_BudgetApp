c456b15b6450623c0cd5e81740e22459b28aca34
KailanB <kailanbates@gmail.com>
2024-12-01 22:16:31 -0700

    Added income type class

---
+++ Sodv2101_Group13_BudgetApp/IncomeType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sodv2101_Group13_BudgetApp
+{
+    internal class IncomeType
+    {
+
+        public int TypeId { get; set; }
+        public string Type {  get; set; }
+
+        public IncomeType(int id, string type)
+        {
+            TypeId = id;
+            Type = type;
+        }
+    }
+}
