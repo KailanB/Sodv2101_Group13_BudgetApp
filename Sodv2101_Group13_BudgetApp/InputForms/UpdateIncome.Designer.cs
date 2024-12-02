a1a96f7d855086986897a5a9fbb6f8228e41b2a9
KailanB <kailanbates@gmail.com>
2024-12-01 22:19:10 -0700

    Added update income form and functionality

---
+++ Sodv2101_Group13_BudgetApp/InputForms/UpdateIncome.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Sodv2101_Group13_BudgetApp.InputForms
+{
+    partial class UpdateIncome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnEditIncome = new Button();
+            btnCancel = new Button();
+            label2 = new Label();
+            comboIncomeType = new ComboBox();
+            label1 = new Label();
+            numAmount = new NumericUpDown();
+            label3 = new Label();
+            txtIncomeName = new TextBox();
+            lblIncomeOutput = new Label();
+            ((System.ComponentModel.ISupportInitialize)numAmount).BeginInit();
+            SuspendLayout();
+            // 
+            // btnEditIncome
+            // 
+            btnEditIncome.BackColor = Color.Plum;
+            btnEditIncome.Location = new Point(235, 198);
+            btnEditIncome.Name = "btnEditIncome";
+            btnEditIncome.Size = new Size(75, 23);
+            btnEditIncome.TabIndex = 11;
+            btnEditIncome.Text = "Edit";
+            btnEditIncome.UseVisualStyleBackColor = false;
+            btnEditIncome.Click += btnEditIncome_Click;
+            // 
+            // btnCancel
+            // 
+            btnCancel.BackColor = Color.Plum;
+            btnCancel.Location = new Point(20, 198);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new Size(75, 23);
+            btnCancel.TabIndex = 10;
+            btnCancel.Text = "Cancel";
+            btnCancel.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(41, 129);
+            label2.Name = "label2";
+            label2.Size = new Size(77, 15);
+            label2.TabIndex = 9;
+            label2.Text = "Income Type:";
+            // 
+            // comboIncomeType
+            // 
+            comboIncomeType.FormattingEnabled = true;
+            comboIncomeType.Location = new Point(141, 126);
+            comboIncomeType.Name = "comboIncomeType";
+            comboIncomeType.Size = new Size(121, 23);
+            comboIncomeType.TabIndex = 8;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(41, 78);
+            label1.Name = "label1";
+            label1.Size = new Size(54, 15);
+            label1.TabIndex = 7;
+            label1.Text = "Amount:";
+            // 
+            // numAmount
+            // 
+            numAmount.Location = new Point(142, 76);
+            numAmount.Maximum = new decimal(new int[] { 9999999, 0, 0, 0 });
+            numAmount.Name = "numAmount";
+            numAmount.Size = new Size(120, 23);
+            numAmount.TabIndex = 6;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(41, 32);
+            label3.Name = "label3";
+            label3.Size = new Size(42, 15);
+            label3.TabIndex = 12;
+            label3.Text = "Name:";
+            // 
+            // txtIncomeName
+            // 
+            txtIncomeName.Location = new Point(141, 29);
+            txtIncomeName.Name = "txtIncomeName";
+            txtIncomeName.Size = new Size(121, 23);
+            txtIncomeName.TabIndex = 13;
+            // 
+            // lblIncomeOutput
+            // 
+            lblIncomeOutput.AutoSize = true;
+            lblIncomeOutput.Location = new Point(20, 237);
+            lblIncomeOutput.Name = "lblIncomeOutput";
+            lblIncomeOutput.Size = new Size(0, 15);
+            lblIncomeOutput.TabIndex = 14;
+            // 
+            // UpdateIncome
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.GradientInactiveCaption;
+            ClientSize = new Size(342, 261);
+            Controls.Add(lblIncomeOutput);
+            Controls.Add(txtIncomeName);
+            Controls.Add(label3);
+            Controls.Add(btnEditIncome);
+            Controls.Add(btnCancel);
+            Controls.Add(label2);
+            Controls.Add(comboIncomeType);
+            Controls.Add(label1);
+            Controls.Add(numAmount);
+            Name = "UpdateIncome";
+            Text = "UpdateIncome";
+            ((System.ComponentModel.ISupportInitialize)numAmount).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnEditIncome;
+        private Button btnCancel;
+        private Label label2;
+        private ComboBox comboIncomeType;
+        private Label label1;
+        private NumericUpDown numAmount;
+        private Label label3;
+        private TextBox txtIncomeName;
+        private Label lblIncomeOutput;
+    }
+}
---
+++ Sodv2101_Group13_BudgetApp/InputForms/UpdateIncome.cs
@@ -0,0 +1,115 @@
+﻿using Sodv2101_Group13_BudgetApp.RepositoryDBContext.DBIncome;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sodv2101_Group13_BudgetApp.InputForms
+{
+    public partial class UpdateIncome : Form
+    {
+
+        private List<IncomeType> incomeTypes = new List<IncomeType>();
+        private IncomeService incomeService = new IncomeService();
+
+        private int incomeId = 0;
+
+        public UpdateIncome()
+        {
+            InitializeComponent();
+            LoadIncomeTypes();
+        }
+
+
+        private void LoadIncomeTypes()
+        {
+            incomeTypes = incomeService.GetIncomeTypeList();
+            if (incomeTypes != null && incomeTypes.Count > 0)
+            {
+                comboIncomeType.DataSource = incomeTypes;
+                comboIncomeType.DisplayMember = "Type";
+                comboIncomeType.ValueMember = "TypeId";
+
+                
+            }
+            else
+            {
+                MessageBox.Show("No income types found.");
+            }
+
+        }
+
+
+
+        public void PopulateInputs(string name, double amount, string type, int id)
+        {
+            txtIncomeName.Text = name;
+            numAmount.Value = (decimal)amount;
+            comboIncomeType.SelectedIndex = comboIncomeType.FindStringExact(type);
+
+            incomeId = id;
+        }
+
+        private void btnEditIncome_Click(object sender, EventArgs e)
+        {
+
+
+
+            bool createdIncome = false;
+            if (numAmount.Value > 0 && txtIncomeName.Text != "")
+            {
+                int incomeTypeId = (int)comboIncomeType.SelectedValue;
+
+                string name = txtIncomeName.Text;
+                double amount = (double)numAmount.Value;
+
+                // if id = 1 type is salary
+                if (incomeTypeId == 1)
+                {
+                    IncomeSalary incomeSalary = new IncomeSalary(name, amount, incomeTypeId);
+                    createdIncome = incomeService.EditIncome(incomeSalary, incomeId);
+                    if (createdIncome)
+                    {
+                        this.DialogResult = DialogResult.OK;
+                        this.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Error editing income!");
+                        this.DialogResult = DialogResult.Cancel;
+                    }
+
+                }
+                else
+                {
+                    IncomeHourly incomeHourly = new IncomeHourly(name, amount, incomeTypeId);
+                    createdIncome = incomeService.EditIncome(incomeHourly, incomeId);
+                    if (createdIncome)
+                    {
+                        this.DialogResult = DialogResult.OK;
+                        this.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Error editing income!");
+                        this.DialogResult = DialogResult.Cancel;
+                    }
+
+                }
+
+            }
+            else
+            {
+                lblIncomeOutput.Text = "Please select income name and value";
+            }
+
+
+
+        }
+    }
+}
